ebf29b015adcd66c09a829266092d2f84143c742
Alvin Bao <55769184+baoalvin1@users.noreply.github.com>
2021-01-23 19:09:23 -0500

    update script

--- app/flask/flaskscript.py
+++ app/flask/flaskscript.py
@@ -1,20 +1,11 @@
 from flask import Flask
 from firebase import Firebase
 from firebase_admin import db
+from config import config
 
-config = {
-  "apiKey": "AIzaSyBZkbEvb2Cixmnekw8OMC6gSXdRNprk6Lw",
-  "authDomain": "vaccinescheduler-6a076.firebaseapp.com",
-  "databaseURL": "https://vaccinescheduler-6a076-default-rtdb.firebaseio.com/",
-  "projectId": "vaccinescheduler-6a076",
-  "storageBucket": "vaccinescheduler-6a076.appspot.com",
-  "messagingSenderId": "463255180859",
-  "appId": "1:463255180859:web:117ae2ee3e01efe0ad2e1e"
-}
 
 firebase = Firebase(config)
 app = Flask(__name__)
-
 
 @app.route('/')
 def getRiskSchedule():
